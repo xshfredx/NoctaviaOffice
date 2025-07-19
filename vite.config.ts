6c06fac84524c5cba000930f8641fc110542344a
xshfredx <fredreynh@gmail.com>
2025-07-19 17:52:05 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
+const env = loadEnv(mode, process.cwd(), '');
+
     return {
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
